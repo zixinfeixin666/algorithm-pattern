a913a00ef97676b86d1d94d82affd103477fa215
zixinfeixin666 <1781935376@qq.com>
2023-09-23 14:59:03 +0800

    commit strStr and subsets by using go

--- exercise/demo.go
+++ exercise/demo.go
@@ -1,1 +1,43 @@
-package exercise
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	//栈
+	stack := make([]int, 0)
+	stack = append(stack, 10)
+	v := stack[len(stack)-1]
+	stack = stack[:len(stack)-1]
+	if len(stack) == 0 {
+
+	}
+
+	//队列
+	queue := make([]int, 0)
+	queue = append(queue, 10)
+	q := queue[0]
+	queue = queue[1:]
+	fmt.Printf("%d", q)
+	if len(queue) == 0 {
+
+	}
+
+	//map
+	m := make(map[string]int)
+	m["hello"] = 1
+	delete(m, "hello")
+	for k, v := range m {
+		print(k, v)
+	}
+	//map 键需要可比较，不能为 slice、map、function
+	//map 值都有默认值，可以直接操作默认值，如：m[age]++ 值由 0 变为 1
+	//比较两个 map 需要遍历，其中的 kv 是否相同，因为有默认值关系，所以需要检查 val 和 ok 两个值
+	fmt.Printf("%d%d", v, stack)
+
+	s := []int{1, 2, 3, 4, 5, 6, 9, 4}
+	sort.Ints(s)
+	println(s)
+}
---
+++ exercise/strStr.go
@@ -0,0 +1,19 @@
+package main
+
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	var i, j int
+	for i = 0; i < len(haystack)-len(needle)+1; i++ {
+		for j = 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+		}
+		if len(needle) == j {
+			return i
+		}
+	}
+	return -1
+}
